fba78817e9d9b870f506df3266971196e6ece3b9
Jales <ponderous1734@gmail.com>
2023-10-02 10:27:42 -0300

    Tentando

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'firstName', 'lastName', 'phone', 'email'
+    ordering = '-id',
+    #list_filter = 'created_date'
+    search_fields = 'id', 'firstName', 'lastName',
+    list_per_page = 10
+    list_max_show_all = 50
+    list_editable = 'phone',
+    list_display_links = 'id',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255, blank=True)
+    createdDate = models.DateTimeField(default=timezone.now)
+    descripition = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%M/')
+
+    def __str__(self) -> str:
+        return f'{self.firstName} {self.lastName}'
+    
